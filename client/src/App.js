f2da2b824bd1fd9293a9caf77de1cb757d610079
Pere <pere.solaclaver@gmail.com>
2019-10-18 08:46:25 +0100

    Create login form

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,21 @@ import Login from "./components/Login";
 import "./styles.scss";
 
 function App() {
+
+  const onSubmitLoginForm = values => {
+    debugger
+  }
+  
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
+        <Route
+          exact
+          path="/"
+          render={props => {
+            return <Login {...props} onSubmitLoginForm={onSubmitLoginForm} />;
+          }}
+        />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
